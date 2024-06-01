be14731550bbe290723c1cb2bc29bd97e3823bd9
gunakarreddy1555 <gunakarreddy15559@gmail.com>
2024-06-01 16:37:39 +0530

    nana it is working

---
+++ MyVillageApp/src/main/java/com/myvillageapp/MyVillageAppApplication.java
@@ -0,0 +1,13 @@
+package com.myvillageapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyVillageAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyVillageAppApplication.class, args);
+	}
+
+}
---
+++ MyVillageApp/src/test/java/com/myvillageapp/MyVillageAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myvillageapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyVillageAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
